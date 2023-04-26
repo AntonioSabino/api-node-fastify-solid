27f0bf30f78ae517f368d92875301c1611b15dbe
Antonio Sabino <aslimajr@outlook.com>
2023-04-26 20:54:07 -0300

    test: realizar testes de registro de um usuário

---
+++ src/services/register.service.spec.ts
@@ -0,0 +1,57 @@
+import { describe, expect, it } from 'vitest'
+import { RegisterService } from './register.service'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users.repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { User } from '@/interfaces/user.interface'
+
+describe('Register Service', () => {
+  it('should be able to register', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerService = new RegisterService(usersRepository)
+
+    const user = await registerService.createUser({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password: '123456',
+    })
+
+    expect(user).toMatchObject<User>
+  })
+
+  it('should hash user password upon registration', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerService = new RegisterService(usersRepository)
+
+    const user = await registerService.createUser({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password: '123456',
+    })
+
+    const isPasswordHashed = await compare('123456', user.password_hash)
+
+    expect(isPasswordHashed).toBe(true)
+  })
+
+  it('should not allow two users with the same email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerService = new RegisterService(usersRepository)
+
+    const email = 'john.doe@gmail.com'
+
+    await registerService.createUser({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(() => {
+      return registerService.createUser({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      })
+    }).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+})
