2a4567e32a94973f7b840a61cab4c7f3dcf3ebc6
Antonio Sabino <aslimajr@outlook.com>
2023-04-26 20:53:22 -0300

    feat: adiciona InMemoryUsersRepository

---
+++ src/repositories/in-memory/in-memory-users.repository.ts
@@ -0,0 +1,26 @@
+import { User, UserRepositoryCreateInput } from '@/interfaces/user.interface'
+import { UsersRepository } from '../users.repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public users: User[] = []
+
+  async findByEmail(email: string) {
+    const user = this.users.find((user) => user.email === email)
+
+    return user || null
+  }
+
+  async create(data: UserRepositoryCreateInput) {
+    const user = {
+      id: '1',
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    }
+
+    this.users.push(user)
+
+    return user
+  }
+}
