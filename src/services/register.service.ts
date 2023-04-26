d9fe536baadc66f8a76ca7c0ce7915b8b164af45
Antonio Sabino <aslimajr@outlook.com>
2023-04-25 23:08:37 -0300

    refactor: refatorei a RegisterService de função para classe, para aplicar inversão de dependência

--- src/services/register.service.ts
+++ src/services/register.service.ts
@@ -1,5 +1,4 @@
 import { prisma } from '@/lib/prisma'
-import { PrismaUsersRepository } from '@/repositories/users.repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterServiceRequest {
@@ -8,28 +7,56 @@ interface RegisterServiceRequest {
   password: string
 }
 
-export async function registerService({
-  name,
-  email,
-  password,
-}: RegisterServiceRequest) {
-  const passwordHash = await hash(password, 8)
+export class RegisterService {
+  private readonly usersRepository: any
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (userWithSameEmail) {
-    throw new Error('User with same email already exists')
+  constructor(usersRepository: any) {
+    this.usersRepository = usersRepository
   }
 
-  const prismaUserRepository = new PrismaUsersRepository()
+  async execute({ name, email, password }: RegisterServiceRequest) {
+    const passwordHash = await hash(password, 8)
 
-  prismaUserRepository.create({
-    name,
-    email,
-    password_hash: passwordHash,
-  })
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new Error('User with same email already exists')
+    }
+
+    this.usersRepository.create({
+      name,
+      email,
+      password_hash: passwordHash,
+    })
+  }
 }
+
+// export async function registerService({
+//   name,
+//   email,
+//   password,
+// }: RegisterServiceRequest) {
+//   const passwordHash = await hash(password, 8)
+
+//   const userWithSameEmail = await prisma.user.findUnique({
+//     where: {
+//       email,
+//     },
+//   })
+
+//   if (userWithSameEmail) {
+//     throw new Error('User with same email already exists')
+//   }
+
+//   const prismaUserRepository = new PrismaUsersRepository()
+
+//   prismaUserRepository.create({
+//     name,
+//     email,
+//     password_hash: passwordHash,
+//   })
+// }
