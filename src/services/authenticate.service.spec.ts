3c1a5094fc6b489ea4e0354484cd59e0fe9ed887
Antonio Sabino <aslimajr@outlook.com>
2023-04-27 16:52:52 -0300

    test: realizar testes para o login do usuário

---
+++ src/services/authenticate.service.spec.ts
@@ -0,0 +1,62 @@
+import { describe, expect, it } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users.repository'
+import { User } from '@/interfaces/user.interface'
+import { AuthenticateService } from './authenticate.service'
+import { hash } from 'bcryptjs'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+
+describe('Authenticate Service', () => {
+  it('should be able to authenticate', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const authenticateService = new AuthenticateService(usersRepository)
+
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password_hash: await hash('123456', 8),
+    })
+
+    const user = await authenticateService.login({
+      email: 'john.doe@gmail.com',
+      password: '123456',
+    })
+
+    expect(user).toMatchObject<User>
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const authenticateService = new AuthenticateService(usersRepository)
+
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password_hash: await hash('123456', 8),
+    })
+
+    await expect(() => {
+      return authenticateService.login({
+        email: 'john.notdoe@gmail.com',
+        password: '123456',
+      })
+    }).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const authenticateService = new AuthenticateService(usersRepository)
+
+    await usersRepository.create({
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      password_hash: await hash('123456', 8),
+    })
+
+    await expect(() => {
+      return authenticateService.login({
+        email: 'john.doe@gmail.com',
+        password: '654321',
+      })
+    }).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
