642c12fe6cc8d8ce2e70c5f4040cee04d0bca532
Antonio Sabino <aslimajr@outlook.com>
2023-05-03 11:27:49 -0300

    test: adiciona teste para listar um usuário pelo id

--- src/http/controllers/users.controller.spec.ts
+++ src/http/controllers/users.controller.spec.ts
@@ -13,11 +13,39 @@ describe('UsersController', () => {
 
   it('should be able to create a new user', async () => {
     const response = await request(app.server).post('/users').send({
-      name: 'John Doe',
-      email: 'john.doe@example.com',
+      name: 'User 1',
+      email: 'user1@example.com',
       password: '123456',
     })
 
     expect(response.statusCode).toBe(201)
+  })
+
+  it('should be able to list a user', async () => {
+    await request(app.server).post('/users').send({
+      name: 'User 2',
+      email: 'user2@example.com',
+      password: '654321',
+    })
+
+    const authResponse = await request(app.server).post('/auth').send({
+      email: 'user2@example.com',
+      password: '654321',
+    })
+
+    const { token } = authResponse.body
+
+    const userResponse = await request(app.server)
+      .get('/users/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(userResponse.statusCode).toBe(200)
+    expect(userResponse.body).haveOwnProperty('user')
+    expect(userResponse.body.user).toEqual(
+      expect.objectContaining({
+        email: 'user2@example.com',
+      }),
+    )
   })
 })
