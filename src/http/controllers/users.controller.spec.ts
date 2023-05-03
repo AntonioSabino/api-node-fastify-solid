a2de37f7128031c14291f92421bcdfc2c9e007fb
Antonio Sabino <aslimajr@outlook.com>
2023-05-03 10:41:43 -0300

    test: adicionar teste e2e para criação de um usuário

--- src/http/controllers/users.controller.spec.ts
+++ src/http/controllers/users.controller.spec.ts
@@ -1,5 +1,23 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('Setup', async () => {
-  console.log('Setup')
+describe('UsersController', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new user', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+  })
 })
