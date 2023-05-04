21936abcbdc3b405107a19bb7c72f148bd0bea21
Antonio Sabino <aslimajr@outlook.com>
2023-05-04 11:43:22 -0300

    refactor: adicionar parametro email para a função

--- src/utils/test/create-and-auth-user.ts
+++ src/utils/test/create-and-auth-user.ts
@@ -1,15 +1,18 @@
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthUser(app: FastifyInstance) {
+export async function createAndAuthUser(
+  app: FastifyInstance,
+  email = 'authenticateduser@example.com',
+) {
   await request(app.server).post('/users').send({
     name: 'Authenticated User',
-    email: 'authenticateduser@example.com',
+    email,
     password: '123456',
   })
 
   const response = await request(app.server).post('/auth').send({
-    email: 'authenticateduser@example.com',
+    email,
     password: '123456',
   })
 
