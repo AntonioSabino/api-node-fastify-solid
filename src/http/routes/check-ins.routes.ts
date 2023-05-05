ef2fbbaf272be68adc69b6dd8e0ea2bfbda86e7e
Antonio Sabino <aslimajr@outlook.com>
2023-05-04 22:00:40 -0300

    feat: criar middleware verifyRole

--- src/http/routes/check-ins.routes.ts
+++ src/http/routes/check-ins.routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '../middlewares/verify-jwt'
 import makeCheckInsController from './factories/check-ins.factory'
+import { verifyRole } from '../middlewares/verify-role'
 
 const checkInsController = makeCheckInsController()
 
@@ -10,8 +11,10 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.get('/count', (req, res) => checkInsController.count(req, res))
   app.get('/history', (req, res) => checkInsController.findMany(req, res))
 
-  app.patch('/:checkInId/validate', (req, res) =>
-    checkInsController.validate(req, res),
+  app.patch(
+    '/:checkInId/validate',
+    { onRequest: [verifyRole('ADMIN')] },
+    (req, res) => checkInsController.validate(req, res),
   )
   app.post('/:gymId', (req, res) => checkInsController.create(req, res))
 }
--- src/http/routes/gyms.routes.ts
+++ src/http/routes/gyms.routes.ts
@@ -1,13 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJwt } from '../middlewares/verify-jwt'
 import makeGymsController from './factories/gyms.factory'
+import { verifyRole } from '../middlewares/verify-role'
 
 const gymsController = makeGymsController()
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
-  app.post('/', (req, res) => gymsController.createGym(req, res))
+  app.post('/', { onRequest: [verifyRole('ADMIN')] }, gymsController.createGym)
   app.get('/search', (req, res) => gymsController.findGymsByName(req, res))
   app.get('/nearby', (req, res) => gymsController.findNearbyGyms(req, res))
 }
